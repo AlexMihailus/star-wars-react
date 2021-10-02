8c486db26ec2e81b6deff4908eda8c5a8f6b5154
Alex Mihailus <mihailus.A.G@gmail.com>
2021-10-02 17:19:37 +0300

    add PropTypes

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 import { withErrorApi } from '@hoc-helpers/withErrorApi';
-import { getApiResource } from '@utils/network';
-import { API_PEOPLE } from '@constants/api';
-import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
 import PeopleList from '@components/PeoplePage/PeopleList';
+import { getApiResource } from '@utils/network';
+import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
+import { API_PEOPLE } from '@constants/api';
 
 import styles from './PeoplePage.module.css';
 
@@ -18,14 +19,14 @@ const PeoplePage = ({ setErrorApi }) => {
             const peopleList = res.results.map(({ name, url }) => {
                 const id = getPeopleId(url);
                 const img = getPeopleImage(id);
-
-                return {
+    
+                return {       
                     id,
                     name,
                     img
                 }
             })
-
+            
             setPeople(peopleList);
             setErrorApi(false);
         } else {
@@ -43,6 +44,10 @@ const PeoplePage = ({ setErrorApi }) => {
             {people && <PeopleList people={people} />}
         </>
     )
+}
+
+PeoplePage.propTypes = {
+    setErrorApi: PropTypes.func
 }
 
 export default withErrorApi(PeoplePage);
